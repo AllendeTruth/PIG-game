5ccefff87ef68533229e24f337daf65d26362ad5
AllendeTruth <nandersizzle@gmail.com>
2018-04-30 08:43:12 -0500

    adding feature that drops player score to 0 if  they roll two 6's in a row

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ GAME RULES:
 
 */
 
+//////////////////////////V.2.0/////////////////////////////////////////
 
+//Goal: add feature that makes player loose entire score if they roll two 6's in a row.
+var scores, roundScore, activePlayer, gamePlaying, lastRoll;
 
-
-
-
-var scores, roundScore, activePlayer, gamePlaying;
+lastRoll = [0,0];
 
 start();
 
@@ -27,6 +27,22 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     if(gamePlaying){
          //1. Random Number
          var dice = Math.floor(Math.random()*6)+1;
+
+
+        lastRoll.push(dice);
+        if(lastRoll.length = 3){
+            lastRoll.shift();
+        }
+        
+        if(lastRoll[0] === lastRoll[1]){
+            scores[activePlayer]= 0;
+            document.getElementById('score-'+activePlayer).textContent = 0;
+        }
+
+        console.log('lastRoll = ' +lastRoll);
+
+
+
          //2. Display the result
              var diceDOM = document.querySelector('.dice')
              diceDOM.style.display = 'block';
@@ -103,6 +119,101 @@ function start(){
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
 };
+
+
+
+//////////////////////////V.1.0/////////////////////////////////////////
+
+
+// var scores, roundScore, activePlayer, gamePlaying;
+
+// start();
+
+
+
+
+
+
+// document.querySelector('.btn-roll').addEventListener('click', function(){
+//     if(gamePlaying){
+//          //1. Random Number
+//          var dice = Math.floor(Math.random()*6)+1;
+//          //2. Display the result
+//              var diceDOM = document.querySelector('.dice')
+//              diceDOM.style.display = 'block';
+//              diceDOM.src='dice-'+dice+'.png';
+     
+//          //3. Update roundscore IF rolled number is not 1
+//              if(dice > 1){
+//                  //add score
+//                  roundScore+= dice;
+//                  document.querySelector('#current-'+activePlayer).textContent = roundScore;
+//              }else{
+                 
+//                  nextPlayer();
+//              }
+//     }
+   
+// });
+
+// document.querySelector('.btn-hold').addEventListener('click', function(){
+//     if(gamePlaying){
+//         //addcurrent score to players global score
+//         scores[activePlayer]+= roundScore;
+//     //update UI
+//         document.getElementById('score-'+activePlayer).textContent = scores[activePlayer];
+
+//         document.querySelector('.dice').style.display = 'none';
+
+//     //check if player won game
+//         if(scores[activePlayer]>=100){
+//             document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
+//             document.querySelector('.dice').style.display = 'none';
+//             document.querySelector('.player-'+activePlayer + '-panel').classList.add('winner');
+//             document.querySelector('.player-'+activePlayer + '-panel').classList.remove('active');
+//             gamePlaying = false;
+            
+//         }else{
+//             nextPlayer();
+//         }
+//     }
+// });
+
+// function nextPlayer(){
+//     activePlayer === 0 ? activePlayer =1: activePlayer =0;
+//             roundScore = 0;
+
+//             document.getElementById(`current-0`).textContent = 0;
+//             document.getElementById(`current-1`).textContent = 0;
+
+//             document.querySelector(".player-0-panel").classList.toggle('active');
+//             document.querySelector(".player-1-panel").classList.toggle('active');
+
+//             document.querySelector('.dice').style.display = 'none';
+// };
+
+// document.querySelector('.btn-new').addEventListener('click', start);
+
+// function start(){
+//     scores = [0,0];
+//     roundScore = 0;
+//     activePlayer = 0;
+//     gamePlaying = true;
+
+//     document.querySelector(".dice").style.display = "none";
+
+//     document.getElementById("score-0").textContent = "0";
+//     document.getElementById("score-1").textContent = "0";
+//     document.getElementById("current-0").textContent = "0";
+//     document.getElementById("current-1").textContent = "0"; 
+//     document.getElementById('name-0').textContent = 'Player 1';
+//     document.getElementById('name-1').textContent = 'Player 2';
+//     document.querySelector('.player-0-panel').classList.remove('winner');
+//     document.querySelector('.player-1-panel').classList.remove('winner');
+//     document.querySelector('.player-0-panel').classList.remove('active');
+//     document.querySelector('.player-1-panel').classList.remove('active');
+//     document.querySelector('.player-0-panel').classList.add('active');
+// };
 
 
 
